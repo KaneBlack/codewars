e9b0fe03fba9e8e62be470277c610efabe2789ce
Kirill <kirilllola@gmail.com>
2021-07-06 17:47:04 +0300

    Worked with 8 kata

---
+++ js/8kata.js
@@ -0,0 +1,47 @@
+// Square(n) Sum
+
+// function squareSum(numbers){
+// 	return numbers.reduce( (a,b) => {
+// 	  return a + (b*b)
+// 	},0)
+//   }
+
+
+
+// Reversed Strings
+
+// function solution(str){
+// 	return str.split('').reverse().join('')
+//   }
+
+
+
+// Multiply
+
+// function multiply(a, b){
+// 	return a * b
+//   }
+
+
+
+// Convert a Number to a String!
+
+// function numberToString(num) {
+// 	return num.toString()
+//   }
+
+
+
+// Convert boolean values to strings 'Yes' or 'No'.
+
+// function boolToWord( bool ){
+// 	return bool ? 'Yes' : 'No'
+//   }
+
+
+
+// Even or Odd
+
+// function even_or_odd(number) {
+//     return number % 2 === 0 ? 'Even' : 'Odd'
+// }
